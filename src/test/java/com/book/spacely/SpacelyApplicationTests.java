c0e5f925160d7813bb56e63fafac365dbdc206a9
renjiearlkurniawan <renjiearlkurniawan.yo@gmail.com>
2025-10-04 20:23:47 +0700

    Initial commit

---
+++ src/test/java/com/book/spacely/SpacelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.spacely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
