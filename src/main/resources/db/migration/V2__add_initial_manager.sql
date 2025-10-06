6e8653d65e7e5177d1107539ec8b6a50c5c34fd7
renjiearlkurniawan <renjiearlkurniawan.yo@gmail.com>
2025-10-06 19:01:38 +0700

    migrate initial data manager

---
+++ src/main/resources/db/migration/V2__add_initial_manager.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(name, email, password, role, created_at) VALUES
+('admin manager', 'admin@gmail.com', '$2a$10$R/9g2/8q9E.062n.6x.j3.xG8.4c.C11.bK6.jL6b/S7H2.L85XG', 'MANAGER', NOW())
